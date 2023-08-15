71166f7c35adb74da50cc85eeec6d2ae3087aa27
aanjakhyasandesh <aanjakhyasandesh8@gmail.com>
2023-08-15 15:20:35 +0545

    l

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '$ut(vct-vmp_m1w_e%qs%zsa5wg%vo+!4&k039hzvz-4g$h1xs'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app']
 
