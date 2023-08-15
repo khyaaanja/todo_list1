6bfe1001e7213a1abaae601f99120b2e4647db24
aanjakhyasandesh <aanjakhyasandesh8@gmail.com>
2023-08-15 15:12:47 +0545

    a

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '$ut(vct-vmp_m1w_e%qs%zsa5wg%vo+!4&k039hzvz-4g$h1xs'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
